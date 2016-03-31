cf0cedbfe5de065706ae7093eb930c0f9ea6b989
Norman <el.norman01@gmail.com>
2016-03-31 15:12:55 -0400

    Ventana prototipo agregada

---
+++ src/main/java/com/normanfernandez/shopassist/shopassist/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.normanfernandez.shopassist.shopassist;
+import com.normanfernandez.shopassist.views.SalesForm;
+
+public class Main 
+{
+    public static void main(String [] args)
+    {
+        SalesForm form = new SalesForm();
+        form.setVisible(true);
+        form.setDefaultCloseOperation(3);
+    }
+}
