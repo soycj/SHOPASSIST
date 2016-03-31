0855dbf5f42709782a4af6cb17cdbb2c854801bc
Norman <el.norman01@gmail.com>
2016-03-31 17:33:41 -0400

    Se agrega modelo inicial.

---
+++ src/main/java/com/normanfernandez/shopassist/models/Item.java
@@ -0,0 +1,61 @@
+package com.normanfernandez.shopassist.models;
+
+public class Item 
+{
+    private int id;
+    private String name;
+    private String description;
+    private String code;
+    private double unitPrice;
+    private String position;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    
+    
+}
